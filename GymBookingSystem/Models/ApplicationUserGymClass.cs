db8fe0481515cd56e3e640257e5f5dd2b1cd91e8
dansaa8 <dansaa8@protonmail.com>
2024-11-13 14:57:51 +0100

    Add Join table-entity between ApplicationUser and GymClass

---
+++ GymBookingSystem/Models/ApplicationUserGymClass.cs
@@ -0,0 +1,9 @@
+﻿namespace GymBookingSystem.Models
+{
+    // Kopplingstabell mellan ApplicationUser och GymClass
+    public class ApplicationUserGymClass
+    {
+        public string ApplicationUserId;
+        public int GymClassId;
+    }
+}
